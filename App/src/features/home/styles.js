d8ef5b8334ae231d960e4ab1d6b0245d59188f74
Jules Dupont <jules.dupont@epitech.eu>
2017-04-02 15:54:01 +0200

    Implementation of the new design (home page), component factoring, extracting the style sheet and iOS compatibility management

--- App/src/features/home/styles.js
+++ App/src/features/home/styles.js
@@ -2,78 +2,114 @@
  * Created by jules on 12/02/17.
  */
 
-import { StyleSheet } from 'react-native';
+import {
+    Platform,
+    StyleSheet,
+    Dimensions,
+} from 'react-native';
 
-const styles = StyleSheet.create({
-    iconStyle: {
-        flex: 0.15,
-        fontSize: 40,
-        color: "rgba(255, 255, 255, 1)",
+const HEADER_MAX_HEIGHT = 180;
+const HEADER_MIN_HEIGHT = Platform.OS === 'ios' ? 64 : 54;
+const AVATAR_SIZE = 70;
+
+const scrollStyle = StyleSheet.create({
+    fill: {
+        flex: 1,
+        backgroundColor: '#233445',
+    },
+    content: {
+        flex: 1,
+    },
+    header: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: '#233445',
+        overflow: 'visible',
+    },
+    headerContainer: {
+        position: 'absolute',
+        flex: 1,
+        flexDirection: 'column',
         justifyContent: 'center',
+        top: 0,
+        left: 0,
+        right: 0,
+        width: null,
+        height: HEADER_MAX_HEIGHT,
+        marginTop: 10,
+    },
+    bar: {
+        height: HEADER_MIN_HEIGHT,
         alignItems: 'center',
-        alignSelf: 'center',
-        marginLeft: 15,
-    },
-
-    iconStyleFA: {
-        flex: 0.15,
-        fontSize: 40,
-        color: "rgba(255, 255, 255, 1)",
-        justifyContent: 'center',
-        marginLeft: 10,
-    },
-
-    ThumbStyle: {
-        flex: 0.15,
         justifyContent: 'center',
     },
-
-    itemTitle: {
+    title: {
         color: 'white',
+        fontSize: 16,
     },
-
-    itemTitleLogout: {
-        flex: 0.75,
-        fontWeight: 'bold',
-        alignSelf: 'center',
+    scrollViewContent: {
+        marginTop: HEADER_MAX_HEIGHT + 12,
     },
-
-    profileTitle: {
-        flex: 0.75,
-        fontWeight: 'bold',
+    row: {
+        height: 40,
+        margin: 16,
+        backgroundColor: '#D3D3D3',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-
-    itemDescr: {
-        fontWeight: 'normal',
-        fontSize: 12,
-        color: 'white',
-        lineHeight: 18,
+    pictureAndGaugesContainer: {
+        flex: 1,
+        flexDirection: 'column',
     },
-
-    arrowStyle: {
-        flex: 0.05,
-        fontSize: 10,
-        alignSelf: 'center',
-        color: 'white',
-        marginRight: 3,
+    pictureAndGauges: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
     },
-
-    trafficLightStyle: {
-        flex: 0.05,
+    leftGauge: {
+        position: 'absolute',
+        top: (HEADER_MAX_HEIGHT / 3) - (100 / 2) + 8,
+        left: Dimensions.get('window').width / 4,
+    },
+    rightGauge: {
+        position: 'absolute',
+        top: (HEADER_MAX_HEIGHT / 3) - (100 / 2) + 8,
+        right: Dimensions.get('window').width / 4
+    },
+    gaugeValue: {
+        color: '#FFFFFF',
         fontSize: 17,
         alignSelf: 'center',
-
     },
-
-    logoutStyle: {
-        flex: 0.15,
-        fontSize: 40,
-        justifyContent: 'center',
-        alignItems: 'center',
+    gaugeDescription: {
+        color: '#c4c4c4',
+        fontSize: 10,
         alignSelf: 'center',
-        marginLeft: 15,
-        color: "#cd5447",
     },
+    picture: {
+        width: AVATAR_SIZE,
+        height: AVATAR_SIZE,
+        resizeMode: 'contain',
+        zIndex: 10,
+    },
+    username: {
+        color: 'white',
+        fontSize: 17,
+        marginBottom: 20,
+        alignSelf: 'center',
+    },
+    blockInfoContainer: {
+        flex: 2,
+        flexDirection: 'row',
+        borderWidth: 0.5,
+        borderColor: '#617487',
+        justifyContent: 'space-around',
+        elevation: 30,
+        backgroundColor: 'black',
+    }
 });
 
-export default styles;
+export default scrollStyle;
